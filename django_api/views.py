fdc07b666355dabc2bc26b49eb1a9c2353922a2b
lohith <lohithveepuri123@gmail.com>
2021-11-16 16:25:26 +0530

    added viewset

--- django_api/views.py
+++ django_api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django_api import serializers
+from rest_framework import viewsets
+
 
 
 class HelloApiView(APIView):
@@ -40,3 +42,43 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({"message":"DELETE"})
+
+class HelloViewSet(viewsets.ViewSet):
+    """test api viewsets"""
+    serializer_class = serializers.HelloSerializer
+    def list(self,request):
+        """return hello message"""
+        a_viewset=[
+            'Uses actions (list, create, retrieve, update, partial_upfate)',
+            'Automatically maps to URLs using routers',
+            'provudes more features with less code'
+
+        ]
+
+        return Response({'message':'Hello', 'a_viewset': a_viewset})
+    def create(self, request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request, pk=None):
+        """Handle getting an object by its Id"""
+        return Response({'http_method':'Get'})
+
+    def update(self,request, pk = None):
+        """Handle updating an object"""
+        return Response({'http_method':'Put'})
+
+    def partial_update(self,request, pk = None):
+        """Handle partial updating an object"""
+        return Response({'http_method':'Patch'})
+
+    def destroy(self,request, pk = None):
+        """Handle removing an object"""
+        return Response({'http_method':'Delete'})
